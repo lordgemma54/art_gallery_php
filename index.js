e1ad83d4b59d543ada4fca1e1230e44c3dc16556
Rahul <rtvconcepts@gmail.com>
2026-04-23 15:28:10 -0400

    database setup

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 window.onload = function () {
   new Ajax.Request("index.php", {
     method: "get",
+    // parameters: { type: "action" },
     onSuccess: showPublicGallery,
     onFailure: ajaxFailed,
     onException: ajaxFailed,
@@ -8,6 +9,15 @@ window.onload = function () {
 };
 
 function showPublicGallery(ajax) {
+  let xml = ajax.responseXML();
+  let tile_ids = xml.getElementsByTagName("image_id");
+
+  for (let i = 0; i < tile_ids.length; i++) {
+    let id = tile_ids[i].nodeValue;
+    let img = document.createElement("img");
+    $("gallery").appendChild(img);
+  }
+
   // on successful load of the page, an ajax object is returned.  parse json or xml object with array of randomized artwork.id so that on each reload a random set of images is displayed.
   // for each artwork.id returned, add a click handler, create an html image element, add styling via unobtrusive js, display tiles in a grid.
   // the xml object is build on the server side
--- index.php
+++ index.php
@@ -1,30 +1,53 @@
 <?php 
   
-$db = //make db connection.
-//since no user input is being passed in, no prepared statement required
-$img_ids_all = rand($db->query("SELECT id FROM artwork;"));
+$host = 'localhost';
+$port = '3306';
+$database = 'art_gallery';
+$user = 'root';
+$password = 'Mr.PouncyChonkers@400';
+
+$db = new PDO("mysql:host=$host;port=$port;dbname=$database", "$user", "$password");
+
+$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// a PDO statement object behaves like a pointer to a db 'cursor' that moves forward through rows,
+// it doesnt store the full dataset in memory until you convert it to a php array. They are read-only
+
+$img_ids_all = $db->query("SELECT id FROM artwork");
+
+$ids = [];
+foreach($img_ids_all as $img_id) {
+    $ids += $img_id;
+    }
+    
+    shuffle($ids);
+
+    print_r($ids);
 
 
-function generate_xml($img_ids_all) {
+echo generate_xml($ids);
+
+function generate_xml($ids) {
     $xmldom = new DOMDocument();
-
+    
     $image = $xmldom->createElement("image"); 
     $xmldom->appendChild($image);
-
     
-    for($i = 0; $i < $img_ids_all; $i++) {
+    
+    for($i = 0; $i < count($ids); $i++) {
         $image_id = $xmldom->createElement("image-id");
-        $image_id_num = $xmldom->createTextNode($img_ids_all[$i]);
+        $image_id_num = $xmldom->createTextNode($ids[$i]);
         $image_id->appendChild($image_id_num);
         
         $image->appendChild($image_id);
-    }
-
+        }
+        
+        
     header("Content-type: text/xml");
     return $xmldom->saveXML();
 
 }
 
-
+// password_hash($password, PASSWORD_DEFAULT);
 
 ?>
