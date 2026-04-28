41368a0681eb40c39e21286d83437f8e8312f88c
Rahul <rtvconcepts@gmail.com>
2026-04-28 19:03:50 -0400

    comment headings broke functionality.  removed

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-/*
-Name: Rahul Venkatesh
-Final Project 
-Course: CPSC 5210 - Web dev 2
-Description: This art gallery app allows a user do create their own gallery to show off their artwork.  The 'artist' (user) can upload files of their artwork and like and comment on the art of other artists.  Likes, comments, and clicking on 'related images', a mini gallery of other works by this same artist, refresh the artwork on the page without reload. */
 window.onload = function () {
   // JS files run on every page they are included on - so this page, runs on every page in which top.html is included in
   let gallery = $("public-gallery");
--- profile.php
+++ profile.php
@@ -1,8 +1,3 @@
-<!-- 
-Name: Rahul Venkatesh
-Final Project 
-Course: CPSC 5210 - Web dev 2
-Description: This art gallery app allows a user do create their own gallery to show off their artwork.  The 'artist' (user) can upload files of their artwork and like and comment on the art of other artists.  Likes, comments, and clicking on 'related images', a mini gallery of other works by this same artist, refresh the artwork on the page without reload. -->
 <?php
 // error_reporting(E_ALL);
 // ini_set('display_errors', 1);
